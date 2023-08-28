2c08b8d89ee6e054af7e73fdd20d30f5255c99b6
nirali393 <143429546+nirali393@users.noreply.github.com>
2023-08-28 11:58:17 -0400

    Day1 Exercise1 - print variable value

---
+++ Day1_Exe_1.py
@@ -0,0 +1,15 @@
+# # -------------------------Exercise 2-------------------------
+# Create two variables (you can freely choose the names) and assign to them following values:
+# 'Python'
+# '3.10'
+# Using these variables and the print() function, print to the console the following text:
+# I love Python 3.10!
+
+# --------------------------------------------------
+
+# two variables for python & 3.0
+lang = 'python' 
+version = '3.10'
+
+# print funtion
+print(f'I love ' +lang+ ' ' + version + '!' )
