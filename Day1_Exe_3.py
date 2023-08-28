07542ee6e51fe019bc88f56ee91b542f51c7cd93
nirali393 <143429546+nirali393@users.noreply.github.com>
2023-08-28 12:00:11 -0400

    Day1 Exercise3 - Area of a circle

---
+++ Day1_Exe_3.py
@@ -0,0 +1,18 @@
+# -------------------Exercise 3--------------------------
+# Write a program that calculates the area of a circle with a radius = 10. Use an approximate value of pi:
+# pi = 3.14
+
+# Print the result to the console as shown below.
+# Expected result: Area: 314.0
+
+# --------------------------------------------------
+
+#variables
+radius = 10
+pi = 3.14
+
+#formula of area of circle where area will store the result
+area = pi*(pow(radius,2))
+
+#printing area
+print(area)
