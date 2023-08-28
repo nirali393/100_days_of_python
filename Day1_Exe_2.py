4af2d16ebe81e91cdadb3cbede446d48922b71dc
nirali393 <143429546+nirali393@users.noreply.github.com>
2023-08-28 11:59:11 -0400

    Day1 Exercise2 - print float variable values

---
+++ Day1_Exe_2.py
@@ -0,0 +1,16 @@
+# # -------------------------Exercise 2-------------------------
+# Assign two variables that store the following values:
+# $ 42.99 - product price (float)
+# 30 lbs - product weight (int)
+
+# Using the f-string formatting style print to the console the following message:
+# Price: $42.99. Weight: 30 lbs.
+
+# --------------------------------------------------
+
+# two variable, for float & int respectively.
+price = float(42.99)
+weight = int(30)
+
+# print function
+print(f'Price: $ {price}. Weight: {weight} lbs')
