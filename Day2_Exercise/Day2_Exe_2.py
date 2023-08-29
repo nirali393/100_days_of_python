bcec0e24157c7b44769bce28b40e5bf921c7332a
Nirali Patel <143429546+nirali393@users.noreply.github.com>
2023-08-29 15:46:02 -0400

    Day2 Exe2 - sum of the first 10 elements in a sequence

---
+++ Day2_Exercise/Day2_Exe_2.py
@@ -0,0 +1,13 @@
+# -------------------Exercise 2-------------------
+# The arithmetic sequence is given with the following formula:a.log(n) = 10 + 4n
+# Calculate the sum of the first ten elements of this sequence. Print the result to the console as shown below.
+# Expected result: The sum of the first 10 elements in a sequence: 320.0
+# --------------------------------------------------
+
+a1 = 14
+a10 = 50
+n = 10
+ 
+s10 = ((a1 + a10) / 2) * n
+
+print(f'The sum of the first 10 elements in a sequence: {s10}')
