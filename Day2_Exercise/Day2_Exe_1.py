046c6968c483dd7398ccc5a51810ed4b3f5ac5c8
Nirali Patel <143429546+nirali393@users.noreply.github.com>
2023-08-29 15:44:37 -0400

    Day2 Exercise1- calculate delta for the quadratic equation

---
+++ Day2_Exercise/Day2_Exe_1.py
@@ -0,0 +1,15 @@
+# -------------------Exercise 1-------------------
+# Write a program that calculates the delta for the quadratic equation:
+# 3x^2 - 4x+1 = 0
+# Print the result to the console as shown below.
+
+# Expected result: Delta: 4
+# --------------------------------------------------
+
+a = 3
+b = -4
+c = 1
+
+delta = b**2 - 4 * a * c
+print(f'Delta: {delta}')
+
