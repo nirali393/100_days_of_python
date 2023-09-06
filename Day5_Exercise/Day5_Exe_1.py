81452060ef5f5bac0f28877e731b9c5b1ea769be
Nirali Patel <nirali.patel@indrorobotics.com>
2023-09-06 06:37:40 -0400

    Day5-Exercise: Print Data Type

---
+++ Day5_Exercise/Day5_Exe_1.py
@@ -0,0 +1,20 @@
+# -------------------Exercise 1-------------------
+# The following variables are given:
+# var1 = None
+# var2 = False
+# var3 = 'True'
+
+# Print each type of variable on a separate line to the console as shown below.
+# Expected result:
+# <class 'NoneType'>
+# <class 'bool'>
+# <class 'str'>
+# # ----------------------------------------------
+
+var1 = None
+var2 = False
+var3 = 'True'
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
