00b14cae0b0728ab2bf72614a66a590bc957fda9
Nirali Patel <nirali.patel@indrorobotics.com>
2023-09-06 06:41:00 -0400

    Day5-Exercise3: String Capilatize

---
+++ Day5_Exercise/Day5_Exe_3.py
@@ -0,0 +1,11 @@
+# -------------------Exercise 3-------------------
+# The following text is given:
+# text = 'python is a popular programming language.'
+
+# Use the appropriate method to replace the first letter of the text with uppercase. Print the result to the console.
+# Expected result: Python is a popular programming language.
+
+# # ----------------------------------------------
+
+text = 'python is a popular programming language.'
+print(text.capitalize())
