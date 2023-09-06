dac325dffbd1d8d05337a0315326ca1a0dac40fa
Nirali Patel <nirali.patel@indrorobotics.com>
2023-09-06 06:39:20 -0400

    Day5-Exercise: Boolean

---
+++ Day5_Exercise/Day5_Exe_2.py
@@ -0,0 +1,11 @@
+# -------------------Exercise 2-------------------
+# Check if the following variable:
+# flag = False
+
+# is an instance of the bool class and print the result to the console.
+# Expected result:True
+# # ----------------------------------------------
+
+flag = False
+
+print(flag == False)
