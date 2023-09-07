b7d5f5854348041cb70be1feef0cbf13499ed068
Nirali Patel <niralipatel3931@gmail.com>
2023-09-07 14:44:57 -0400

    Day7 Execirse2 - string lower()

---
+++ Day7_Exercise/Day7_Exe_2.py
@@ -0,0 +1,11 @@
+# -------------------Exercise 2-------------------
+# The following text is given:
+# text = '100 Days of Code'
+
+# Using the appropriate method convert all letters to lowercase. Print the result to the console.
+# Expected result: 100 days of code
+# --------------------------------------------------
+
+text = '100 Days of Code'
+
+print(text.lower())
