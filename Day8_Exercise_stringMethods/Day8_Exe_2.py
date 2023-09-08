6859939a89f1714bba104aaf19bcaefa83ecff75
Nirali Patel <niralipatel3931@gmail.com>
2023-09-08 16:27:17 -0400

    set split()

---
+++ Day8_Exercise_stringMethods/Day8_Exe_2.py
@@ -0,0 +1,15 @@
+# -------------------Exercise 2-------------------
+# The following text is given:
+# text = """Python is a general-purpose language.
+# Python is popular."""
+
+# Using the appropriate method, split the text into sentences. Print the result as a list to the console.
+
+# Expected result:
+# ['Python is a general-purpose language.', 'Python is popular.']
+# --------------------------------------------------
+
+text = """Python is a general-purpose language.
+Python is popular."""
+
+print(text.splitlines())
