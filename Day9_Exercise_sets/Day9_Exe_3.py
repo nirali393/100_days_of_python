aa618017eb3ab6c38f9de4a09edde229f7861d2f
Nirali Patel <niralipatel3931@gmail.com>
2023-09-08 16:33:57 -0400

    Day9 Exe3 - set symmetrical difference

---
+++ Day9_Exercise_sets/Day9_Exe_3.py
@@ -0,0 +1,17 @@
+# -------------------Exercise 1-------------------
+# In mathematics, the symmetric difference of two sets is the set of elements which are in either of the sets, but not in their intersection.
+
+# Two following sets are given:
+# A = {2, 4, 6, 8}
+# B = {4, 10}
+
+# Using the appropriate method, extract the symmetrical difference of sets A and B and print the result to the console as shown below.
+
+# Expected result:
+# Symmetric difference: {2, 6, 8, 10}
+# --------------------------------------------------
+
+A = {2, 4, 6, 8}
+B = {4, 10}
+
+print(A.symmetric_difference(B))
