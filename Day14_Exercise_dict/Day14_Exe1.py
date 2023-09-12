052ed64fd2f363cb8833bdc6bfcfeeb6842f4aa4
Nirali Patel <niralipatel3931@gmail.com>
2023-09-12 18:33:14 -0400

    Day14 Exe1 - Dict print all the keys

---
+++ Day14_Exercise_dict/Day14_Exe1.py
@@ -0,0 +1,20 @@
+# -------------------Exercise 1--------------------
+# The following dictionary is given:
+# capitals = {
+#     'USA': 'Washington',
+#     'Germany': 'Berlin',
+#     'Austria': 'Vienna'
+# }
+# Use the appropriate method to extract all keys from the capitals dictionary and print to the console.
+
+# Expected result:
+# dict_keys(['USA', 'Germany', 'Austria'])
+# --------------------------------------------------
+
+capitals = {
+    'USA': 'Washington',
+    'Germany': 'Berlin',
+    'Austria': 'Vienna',
+}
+
+print(capitals.keys())
