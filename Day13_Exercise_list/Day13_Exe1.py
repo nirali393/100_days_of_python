5d77b9c1645945473c5fb4b0de34e5a4b1c9a1d1
Nirali Patel <niralipatel3931@gmail.com>
2023-09-12 17:45:01 -0400

    Day13 Exe1 - concatenate List

---
+++ Day13_Exercise_list/Day13_Exe1.py
@@ -0,0 +1,19 @@
+# -------------------Exercise 1--------------------
+# The following list represents order ids for a given day:
+# day1 = ['3984', '9042', '4829', '2380']
+
+# Using the appropriate method, extend this list to the next day:
+
+# day2 = ['4231', '5234', '1345', '2455']
+
+# Print the result to the console.
+
+# Expected result:
+# ['3984', '9042', '4829', '2380', '4231', '5234', '1345', '2455']
+# -------------------------------------------------
+
+day1 = ['3984', '9042', '4829', '2380']
+day2 = ['4231', '5234', '1345', '2455']
+
+day3 = day1+day2
+print(day3)
