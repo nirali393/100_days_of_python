9640452018d3e67c076017db4256b5a51039fb6d
Nirali Patel <niralipatel3931@gmail.com>
2023-09-12 16:57:05 -0400

    Day12 Exe2 - List count

---
+++ Day12_Exercise_Lists/Day12_Exe_2.py
@@ -0,0 +1,12 @@
+# -------------------Exercise 2-------------------
+# The following list is given:
+# idx = ['001', '002', '001', '003', '001']
+
+# Using the appropriate method count the occurrences of '001'. Print the result to the console as shown below.
+# Expected result: Number of occurrences: 3
+#-------------------------------------------------
+
+idx = ['001', '002', '001', '003', '001']
+
+l = idx.count('001')
+print(f'Number of occurrences: {l}')
