cf4b42b5f85eded14a0db701d16f7bd4e91bb49b
Nirali Patel <niralipatel3931@gmail.com>
2023-09-12 10:18:34 -0400

    Day10 Exe2 - tuples join 2 tuple

---
+++ Day10_Exercise_sets/Day10_Exe_2.py
@@ -0,0 +1,14 @@
+# -------------------Exercise 2-------------------
+# Two following tuples are given:
+# dji1 = ('AAPL.US', 'IBM.US', 'MSFT.US')
+# dji2 = ('HD.US', 'GS.US', 'NKE.US')
+
+# Combine these tuples into one as shown below and print the result to the console.
+# Expected result:('AAPL.US', 'IBM.US', 'MSFT.US', 'HD.US', 'GS.US', 'NKE.US')
+# --------------------------------------------------
+
+dji1 = ('AAPL.US', 'IBM.US', 'MSFT.US')
+dji2 = ('HD.US', 'GS.US', 'NKE.US')
+
+dji3 = dji1+dji2
+print(dji3)
