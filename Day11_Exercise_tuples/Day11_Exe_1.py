af8cd5e314af737653fb4c876cf48e4a98d0e2ae
Nirali Patel <niralipatel3931@gmail.com>
2023-09-12 10:41:28 -0400

    Day11 Exe1 - concatinate tuples

---
+++ Day11_Exercise_tuples/Day11_Exe_1.py
@@ -0,0 +1,15 @@
+# -------------------Exercise 3-------------------
+# The following tuples are given:
+# dji1 = ('AAPL.US', 'IBM.US', 'MSFT.US')
+# dji2 = ('HD.US', 'GS.US', 'NKE.US')
+
+# Nest these tuples into one tuple as shown below and print the result to the console.
+
+# Expected result:
+# (('AAPL.US', 'IBM.US', 'MSFT.US'), ('HD.US', 'GS.US', 'NKE.US'))
+# --------------------------------------------------
+
+dji1 = ('AAPL.US', 'IBM.US', 'MSFT.US')
+dji2 = ('HD.US', 'GS.US', 'NKE.US')
+
+print(f'{dji1}, {dji2}')
