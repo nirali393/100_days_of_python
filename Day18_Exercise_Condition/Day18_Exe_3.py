fbc38cc0bd92bfb61119d324180c90e20cef20cd
Nirali Patel <niralipatel3931@gmail.com>
2023-09-15 21:28:26 -0400

    Day18 Exe3 - If else

---
+++ Day18_Exercise_Condition/Day18_Exe_3.py
@@ -0,0 +1,16 @@
+# -------------------Exercise 3----------------------------
+# The following variable is given:
+# number = 1.0
+
+# Test whether the variable is an instance of the built-in class int. Print 'YES' if true, 'NO' if false.
+
+# Expected result:
+# NO
+# ---------------------------------------------------------
+
+number = 1.0
+
+if isinstance(number, int):
+    print('YES')
+else: 
+    print('NO')
