74780da6629d20ada803bbe1818f17d009c99cd6
Nirali Patel <niralipatel3931@gmail.com>
2023-10-18 16:40:36 -0400

    Adding Day28 Exe1

--- Day28_Exercise_file/Day28_Exe1.py
+++ Day28_Exercise_file/Day28_Exe1.py
@@ -6,9 +6,25 @@
 # 3-day average closing price: xxx.xx
 # ----------------------------------------------------------
 
+
+import pandas as pd
+
 try:
-    f = open("playway.txt", "r")
-    print(f.read())
-    f.readlines()
+    with open("playway.txt", "r") as f:
+        first_line = True
+        temp_value = 0
+        for line in f:
+            if first_line:
+                first_line = False
+                continue
+            values = line.split(',')
+            value_at_index_4 = values[4]
+            temp_value = temp_value + int(value_at_index_4)
+
+        reault = int(temp_value)/3
+        print(f'3-day average closing price:{reault:.2f}')
+            
+
 except FileNotFoundError:
     print("File not found")
+    
