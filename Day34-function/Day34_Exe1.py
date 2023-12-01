47217fb52feab824db8ab7950807ca526d8941a3
Nirali Patel <niralipatel3931@gmail.com>
2023-12-01 12:54:39 -0500

    Adding Day34- function, new learn map()

---
+++ Day34-function/Day34_Exe1.py
@@ -0,0 +1,27 @@
+# -------------------Exercise 1----------------------------
+# Implement a function map_longest() that accepts the list of words and return the length of the longest word in this list.
+# [IN]: map_longest(['python', 'sql'])
+# [OUT]: 6
+
+
+# [IN]: map_longest(['java', 'sql', 'r'])
+# [OUT]: 4
+# ---------------------------------------------------------
+
+# taking array from user and stored in l
+arr = input("Enter Words in list seperated by a comma: ")
+l = list(map(str,arr.split(',')))     # map function will itrate on list and seperate by ,
+
+
+# functio
+def map_longest():
+
+    # len will count the length of each elemnt in alist and stored in length_l
+    length_l=list(map(len, l))
+
+    # max function to get the maximum no. from the list length_l
+    result = max(length_l)
+    print(result)
+
+# calling the function
+map_longest()
