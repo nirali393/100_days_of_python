eca73d1334f43393b5fc311080404c7aa0ef73fb
Nirali Patel <niralipatel3931@gmail.com>
2023-12-04 13:36:36 -0500

    Day36 functions

---
+++ Day36_function/Day36_Exe3.py
@@ -0,0 +1,24 @@
+# -------------------Exercise 3----------------------------
+# The following function is given:
+
+
+
+# def function(*args, **kwargs):
+#     print(args, kwargs)
+
+
+# Call the function in the order given:
+
+
+
+# function(3, 4)
+# function(x=3, y=4)
+# function(1, 2, x=3, y=4)
+# ---------------------------------------------------------
+
+def function(*args, **kwargs):
+    print(args, kwargs)
+
+function(3, 4)
+function(x=3, y=4)
+function(1, 2, x=3, y=4)
