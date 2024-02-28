9f44f4f44498c7e479873e58886cccb3cd866723
Nirali Patel <niralipatel3931@gmail.com>
2024-02-28 04:38:57 -0500

    Adding Day41 Exec 2

---
+++ Day41_set_comprehension/Day41_Exe2.py
@@ -0,0 +1,13 @@
+# -------------------Exercise 1---------------------------- 
+# The following text is given:
+# desc = "Playway: Playway is a producer of computer games."
+
+# Change all characters to lowercase, remove the colon, period and then split the text into words.
+
+# Create a set of unique words and print the length of this set to the console.
+# ---------------------------------------------------------
+
+desc = "Playway: Playway is a producer of computer games."
+words = desc.lower().replace(':','').replace('.','').split(' ')
+print(len(set(words)))
+
