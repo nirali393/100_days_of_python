0f9c250511cab671e67c6c060fbbaa78a74ec329
Nirali Patel <niralipatel3931@gmail.com>
2024-02-29 16:50:35 -0500

    space

--- Day41_set_comprehension/Day41_Exe3.py
+++ Day41_set_comprehension/Day41_Exe3.py
@@ -8,9 +8,6 @@
 # Probability: 0.22
 # ---------------------------------------------------------
 
-
 omega = {(i,j): pow(i,2)+pow(j,2) for i in range(1,7) for j in range(1,7)}
 occurance_of_event = len([value for value in omega.values() if value >=45])
-prob = 0
 print(f"Probability: {round(occurance_of_event/len(omega),2)}")
-
