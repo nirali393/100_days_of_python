a46831541daf156777c9a89c969689de006d81fb
Nirali Patel <niralipatel3931@gmail.com>
2024-03-11 15:39:08 -0400

    Day48 Exe1

---
+++ Day48_datetime_and_regex/Day48_Exe1.py
@@ -0,0 +1,13 @@
+# -------------------Exercise 2---------------------------- 
+# Using the datetime built-in module, calculate the difference for dates (date 2 - date 1):
+# date 1: 2020-06-01
+# date 2: 2020-07-18
+
+# Print the result to the console as shown below.
+# Expected result:
+# 47 days, 0:00:00
+# ---------------------------------------------------------
+import datetime
+x = datetime.datetime.now()
+y = datetime.datetime(1998, 8, 30)     
+print(f"I exist on earth from {x-y}")
