999a8d51b91cf76b28091857071552db48d02a17
Nirali Patel <niralipatel3931@gmail.com>
2024-03-12 16:43:49 -0400

    Day48 Exe3

---
+++ Day48_datetime_and_regex/Day48_Exe3.py
@@ -0,0 +1,15 @@
+# -------------------Exercise 2---------------------------- 
+# Using the built-in module for regular expressions, find all alphanumeric characters in the following text:
+# string = '!@#$%^&45wc'
+
+# Print the result to the console.
+
+# Tip: Use the findall() function and the regular expression '\w'
+# Expected result:
+# ['4', '5', 'w', 'c']
+# ---------------------------------------------------------
+import re
+string = '!@#$%^&45wc'
+
+alpha_nu = re.findall(pattern=r"\w", string=string)
+print(alpha_nu)
