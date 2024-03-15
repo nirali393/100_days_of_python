515736138fbd81821c8702c1b749552cafe63de8
Nirali Patel <niralipatel3931@gmail.com>
2024-03-15 13:45:58 -0400

    Day49 Exe3

---
+++ Day49_regex_and_string/Day49_Exe3.py
@@ -0,0 +1,10 @@
+# -------------------Exercise 3---------------------------- 
+# Using the string built-in module, print a string of lowercase and uppercase letters to the console.
+
+# Expected result:
+# abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+# ---------------------------------------------------------
+
+import string
+ 
+print(string.ascii_letters)
