6e2373426703b4336ff72d83bdd738fa6a582b24
Nirali Patel <niralipatel3931@gmail.com>
2024-03-25 18:39:58 -0400

    Add Exe 2

---
+++ Day50_collections_and_math/Day50_Exe2.py
@@ -0,0 +1,11 @@
+# -------------------Exercise 1---------------------------- 
+# Using the math built-in module, implement the function called sigmoid() expressed by the formula:
+# f(x) = 1/[1+(e)^-x)
+# Note: All you have to do is implement the function.
+# ---------------------------------------------------------
+import math
+ 
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+sim = sigmoid(3)
+print(sim)
